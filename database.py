4c73bd2e7cc58bf6c837037f160b884afa2681ff
mariyyah <ma_ria111@icloud.com>
2025-06-16 18:52:48 +0300

    add to requirement

--- database.py
+++ database.py
@@ -3,17 +3,10 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# تحميل متغيرات البيئة من ملف .env
-load_dotenv()
-
-# قراءة الرابط
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# تحقق من وجود الرابط
 if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL not found. تأكدي إنه معرف في .env")
+    raise ValueError("DATABASE_URL not found. تأكدي إنه معرف في بيئة التشغيل")
 
-# إنشاء الاتصال
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
