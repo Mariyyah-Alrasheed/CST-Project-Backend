6bf5709c3a355e713cd9c7d7f8e1e6820d98bcaf
mariyyah <ma_ria111@icloud.com>
2025-06-16 15:55:21 +0300

    add database file

--- database.py
+++ database.py
@@ -1,15 +1,13 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-URL_DATABASE = "postgresql://postgres:Ma_ria111@localhost:5432/StopRequestsDatabase"
+# قراءة رابط قاعدة البيانات من متغير البيئة
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(URL_DATABASE)
+# إنشاء الاتصال
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-
-
